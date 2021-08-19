320159317b33be171ca68096844dc24903b6e335
Tulikasharma <tulikasharma368@gmail.com>
2021-08-20 00:25:46 +0530

    UC3, refractored to edit contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,6 +1,7 @@
 package addressBookCollections;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBook {
 	
@@ -16,8 +17,46 @@ public class AddressBook {
 	
 	public void addContact(Contact contact) {
 		addBook.add(contact);
-		addBook.add(contact);
-		System.out.println(addBook.size());
+		//System.out.println(addBook.size());
+	}
+
+	public void editContact(String fName) {
+		Scanner sc = new Scanner(System.in);
+		String name = "" ;
+		for(Contact contact : addBook) {
+			name = contact.getfName() + contact.getlName();
+			if(fName.equals(name)) {
+				System.out.println("Choose the value to be edited");
+				System.out.println("Press 1 to edit Address");
+				System.out.println("Press 2 to edit City");
+				System.out.println("Press 3 to edit State");
+				System.out.println("Press 4 to edit Zip Code");
+				System.out.println("Press 5 to edit Phone Numbe");
+				System.out.println("Press 6 to edit E-Mail");
+				int choice = sc.nextInt();
+				sc.nextLine();
+				switch (choice) {
+				case 1:
+					System.out.println("Enter the new Address");
+					String newAddress = sc.nextLine();
+					contact.setAddress(newAddress);
+					String val = contact.getAddress();
+					System.out.println(val);
+					break;
+				case 2:
+					System.out.println("Enter the new City");
+					String newCity = sc.nextLine();
+					contact.setAddress(newCity);
+					break;
+					
+
+				}
+				
+				
+			}
+		}
+		
+		
 	}
 
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -15,30 +15,55 @@ public class AddressBookMain {
 		long phNumber;
 		String email;
 		
-		
+		//I am using fName 2 times, should i declare it at both places
+		//using nextLine to input data creates problem(eg,add nextLine in line 36 )
 		AddressBook addressBook = new AddressBook();
 		
-		Scanner S = new Scanner(System.in);
 		Scanner sc = new Scanner(System.in);
-		System.out.println("First Name");
-		fName=sc.nextLine();
-		System.out.println("Last Name");
-		lName=sc.nextLine();
-		System.out.println("Address");
-		address=sc.nextLine();
-		System.out.println("City");
-		city=sc.nextLine();
-		System.out.println("State");
-		state=sc.nextLine();
-		System.out.println("Zip Code");
-		zipCode=sc.nextInt();
-		System.out.println("Phone Number");
-		phNumber=sc.nextLong();
-		System.out.println("E-mail");
-		email=sc.next();
 		
-		Contact contact = new Contact(fName, lName, address, city, state, zipCode, phNumber, email);
-		addressBook.addContact(contact);
+		int choice = 0;
 		
+		while(choice!=10) {
+			System.out.println("Press 1 to add contact");
+			System.out.println("Press 2 to edit contact");
+			System.out.println("Press 10 to exit");
+			
+			choice = sc.nextInt();
+			
+			switch (choice) {
+			case 1: {
+				System.out.println("First Name");
+				fName=sc.next();
+				System.out.println("Last Name");
+				lName=sc.next();
+				System.out.println("Address");
+				address=sc.next();
+				System.out.println("City");
+				city=sc.next();
+				System.out.println("State");
+				state=sc.next();
+				System.out.println("Zip Code");
+				zipCode=sc.nextInt();
+				System.out.println("Phone Number");
+				phNumber=sc.nextLong();
+				System.out.println("E-mail");
+				email=sc.next();
+				
+				Contact contact = new Contact(fName, lName, address, city, state, zipCode, phNumber, email);
+				addressBook.addContact(contact);
+				break;
+			}
+			case 2:{
+				System.out.println("Enter the first name of the person whose details need to be edited");
+				fName=sc.next();
+				System.out.println("Enter the last name of the person whose details need to be edited");
+				fName+=sc.next();
+				addressBook.editContact(fName);
+			}
+				
+			
+			}	
+		}
+		sc.close();		
 	}
 }
